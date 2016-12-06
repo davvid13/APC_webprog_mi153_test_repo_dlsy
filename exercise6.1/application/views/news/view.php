3dab124c685311b866af3b9dbe9b71192ef59258
Reinan Meer <reinanpmeer@gmail.com>
2016-12-06 11:32:22 +0800

    Didnt appear on my computer. Found out that i was not able to push on this PC

---
+++ exercise6.1/application/views/news/view.php
@@ -0,0 +1,5 @@
+Firstname: <?php echo $news_item['first_name']; ?>
+Lastname: <?php echo $news_item['last_name']; ?>
+Nickname: <?php echo $news_item['nick_name']; ?>
+Email: <?php echo $news_item['email']; ?>
+Gender: <?php echo $news_item['gender']; ?>
