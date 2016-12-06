d09b599474fdcba8d065555b5b77c03a3e8e1ac3
Reinan Meer <reinanpmeer@gmail.com>
2016-12-06 12:35:13 +0800

    Edited something on sample code/application/controllers/News.php

--- exercise6.2/sample code/application/controllers/News.php
+++ exercise6.2/sample code/application/controllers/News.php
@@ -78,8 +78,11 @@ class News extends CI_Controller {
         $data['title'] = 'Edit a news item';        
         $data['news_item'] = $this->news_model->get_news_by_id($id);
         
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('text', 'Text', 'required');
+		$this->form_validation->set_rules('first_name', 'Firstname', 'required');
+        $this->form_validation->set_rules('last_name', 'Lastname', 'required');
+		$this->form_validation->set_rules('nick_name', 'Nickname', 'required');
+		$this->form_validation->set_rules('email', 'E-mail', 'required');
+		$this->form_validation->set_rules('gender', 'Gender', 'required');
  
         if ($this->form_validation->run() === FALSE)
         {
