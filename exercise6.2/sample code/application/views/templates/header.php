0a88b1fbab243a84bef7a58ac413c4f97cc901b1
Reinan Meer <reinanpmeer@gmail.com>
2016-12-06 12:47:02 +0800

    edited sample code/application/views/templates/header.php

--- exercise6.2/sample code/application/views/templates/header.php
+++ exercise6.2/sample code/application/views/templates/header.php
@@ -4,7 +4,6 @@
 	</head>
 	<body>
 		<h1>WEBPROG_EXERCISE 6</h1>
-		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
 		<h2>What You Do Today Can Improve All Your Tomorrow -Ralph Martson</h2>	
 				<img class="me" src="picture.jpg" alt="Me">
 				
@@ -19,3 +18,4 @@
 					<th>Davvid</th>
 				</tr>	
 			</table>
+		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
