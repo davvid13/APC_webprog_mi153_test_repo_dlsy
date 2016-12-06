6a702d24a5e53149aff1a4171972d026692ab74b
Reinan Meer <reinanpmeer@gmail.com>
2016-12-06 12:44:25 +0800

    Edited sample code/application/views/templates/header.php

--- exercise6.2/sample code/application/views/templates/header.php
+++ exercise6.2/sample code/application/views/templates/header.php
@@ -1,7 +1,21 @@
 <html>
 	<head>
-		<title>CodeIgniter Tutorial</title>
+		<title>My Final Ex6</title>
 	</head>
 	<body>
-		<h1>Simple CRUD</h1>
+		<h1>WEBPROG_EXERCISE 6</h1>
 		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+		<h2>What You Do Today Can Improve All Your Tomorrow -Ralph Martson</h2>	
+				<img class="me" src="picture.jpg" alt="Me">
+				
+			<table>
+				<tr>
+					<th>Fullname:</th>
+					<th>Davvid Sy</th>
+				</tr>
+				
+				<tr>
+					<th>Nickname:</th>
+					<th>Davvid</th>
+				</tr>	
+			</table>
