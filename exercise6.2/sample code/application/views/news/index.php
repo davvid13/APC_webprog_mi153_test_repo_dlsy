1e1cd40712ef367fa2003befff4341a12f9eafda
Reinan Meer <reinanpmeer@gmail.com>
2016-12-06 12:55:10 +0800

    edited sample code/application/views/news/index.php

--- exercise6.2/sample code/application/views/news/index.php
+++ exercise6.2/sample code/application/views/news/index.php
@@ -2,14 +2,19 @@
  
 <table border='1' cellpadding='4'>
     <tr>
-        <td><strong>Title</strong></td>
-        <td><strong>Content</strong></td>
-        <td><strong>Action</strong></td>
+        <td><strong>Firstname</strong></td>
+        <td><strong>Lastname</strong></td>
+        <td><strong>Nickname</strong></td>
+		<td><strong>E-mail</strong></td>
+		<td><strong>Gender</strong></td>
     </tr>
 <?php foreach ($news as $news_item): ?>
         <tr>
-            <td><?php echo $news_item['title']; ?></td>
-            <td><?php echo $news_item['text']; ?></td>
+            <td><?php echo $news_item['first_name']; ?></td>
+            <td><?php echo $news_item['last_name']; ?></td>
+			<td><?php echo $news_item['nick_name']; ?></td>
+			<td><?php echo $news_item['email']; ?></td>
+			<td><?php echo $news_item['gender']; ?></td>
             <td>
                 <a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View</a> | 
                 <a href="<?php echo site_url('news/edit/'.$news_item['id']); ?>">Edit</a> | 
