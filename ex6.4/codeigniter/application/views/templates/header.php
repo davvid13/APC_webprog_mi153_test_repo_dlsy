68c4353c9bcb07a64e924cbd2e72dd9cafbed910
Davvid <davvid19996@gmail.com>
2016-12-13 11:47:44 +0800

    Another source code

---
+++ ex6.4/codeigniter/application/views/templates/header.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+		<title>DATABASE USING CODEIGNITER</title>
+	</head>
+	<body>
+		<h1>Simple CRUD</h1>
+		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add</a></p>
