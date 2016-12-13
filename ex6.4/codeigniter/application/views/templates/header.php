4e16be5a864e52f0e8725b86e03dabe2a7ee80b9
Davvid <davvid19996@gmail.com>
2016-12-13 12:26:17 +0800

    edited codeigniter/application/views/templates/header.php

--- ex6.4/codeigniter/application/views/templates/header.php
+++ ex6.4/codeigniter/application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
-		<title>DATABASE USING CODEIGNITER</title>
+		<title>My Website</title>
 	</head>
 	<body>
-		<h1>Simple CRUD</h1>
+		<h1>Exercise 6.4</h1>
 		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add</a></p>
