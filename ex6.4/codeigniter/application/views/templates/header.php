a39afd91718b8b33c16a3b719a3947c06e669bca
Davvid <davvid19996@gmail.com>
2016-12-13 12:36:57 +0800

    Added: h2 me picture website motto Fixed some color issues

--- ex6.4/codeigniter/application/views/templates/header.php
+++ ex6.4/codeigniter/application/views/templates/header.php
@@ -1,8 +1,8 @@
 <html>
 	<head>
 	<style>
-				h1, h2 				{text-align:center; 
-								 color:white;
+			h1, h2 				{text-align:center; 
+								 color:black;
 								 font-family:courier new;}
 					
 			img.me 				{float:right;
@@ -10,10 +10,10 @@
 					
 			table 				{width:100%;}
 			
-			th					{color:white;
+			th					{color:black;
 								 font-family:georgia}
 					 
-			p					{color:white;
+			p					{color:black;
 								 font-family:georgia}
 								 					 
 			img.basketball		{width:130px;}
@@ -49,91 +49,12 @@
 								 height:100px;}
 			img.mangafox		{width:130px;
 								 height:110px;}
-			.error  			{color: #FF0000;}
-			
-			{
- margin:0;
- padding:0;
-}
-body
-{
- background:black;
- font-family:"Courier New", Courier, monospace;
-}
-#header
-{
- width:100%;
- height:50px;
- background:#00a2d1;
- color:#f9f9f9;
- font-family:"Lucida Sans Unicode", "Lucida Grande", sans-serif;
- font-size:35px;
- text-align:center;
-}
-#header a
-{
- color:#fff;
- text-decoration:blink;
-}
-#body
-{
- margin-top:50px;
-}
-table
-{
- width:80%;
- font-family:Tahoma, Geneva, sans-serif;
- font-weight:bolder;
- color:#999;
- margin-bottom:80px;
-}
-table a
-{
- text-decoration:none;
- color:#00a2d1;
-}
-table,td,th
-{
- border-collapse:collapse;
- border:solid #d0d0d0 1px;
- padding:20px;
-}
-table td input
-{
- width:97%;
- height:35px;
- border:dashed #00a2d1 1px;
- padding-left:15px;
- font-family:Verdana, Geneva, sans-serif;
- box-shadow:0px 0px 0px rgba(1,0,0,0.2);
- outline:none;
-}
-table td input:focus
-{
- box-shadow:inset 1px 1px 1px rgba(1,0,0,0.2);
- outline:none;
-}
-table td button
-{
- border:solid #f9f9f9 0px;
- box-shadow:1px 1px 1px rgba(1,0,0,0.2);
- outline:none;
- background:#00a2d1;
- padding:9px 15px 9px 15px;
- color:#f9f9f9;
- font-family:Arial, Helvetica, sans-serif;
- font-weight:bolder;
- border-radius:3px;
- width:49.5%;
-}
-table td button:active
-{
- position:relative;
- top:1px;
-}
-	<style/>
+	</style>
 		<title>My Website</title>
 	</head>
 	<body>
 		<h1>Exercise 6.4</h1>
 		<p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add</a></p>
+		
+		<h2>What You Do Today Can Improve All Your Tomorrow -Ralph Martson</h2>	
+				<img class="me" src="picture.jpg" alt="Me">
