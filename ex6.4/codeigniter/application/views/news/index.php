a73c349240ff6b532aeb77c6fcb15a77448d921b
Davvid <davvid19996@gmail.com>
2016-12-13 13:05:46 +0800

    Added tons of picture in index.php

--- ex6.4/codeigniter/application/views/news/index.php
+++ ex6.4/codeigniter/application/views/news/index.php
@@ -1,58 +1,30 @@
 <!DOCTYPE html>
 <head>
-<style>
-			h1, h2 				{text-align:center; 
-								 color:white;
-								 font-family:courier new;}
-					
-			img.me 				{float:right;
-								 width:100px;}
-					
-			table 				{width:100%;}
-			
-			th					{color:white;
-								 font-family:georgia}
-					 
-			p					{color:white;
-								 font-family:georgia}
-								 					 
-			img.basketball		{width:130px;}
-			img.video			{width:130px;
-								 height:81.2px;}
-			img.books			{width:137px;
-								 height:81px;}
-			img.food			{width:136px;
-								 height:81px;}
-			img.dog				{width:130px;
-								 height:81px;}
-			img.friends			{width:130px;
-								 height:83px;}
-			img.dance			{width:133px;
-								 height:85px;}
-			img.csgo			{width:138px;
-								 height:85px;}
-			img.youtube			{width:130px;
-								 height:105px;}
-			img.google			{width:130px; 
-								 height:95px;}
-			img.reddit			{width:130px;
-								 height:100px;}
-			img.nba				{width:130px;
-								 height:100px;}			
-			img.hltv			{width:132px;
-								 height:100px;}
-			img.facebook		{width:125px;
-								 height:100px;}
-			img.gag				{width:130px;
-								 height:100px;}
-			img.moodle			{width:130px;
-								 height:100px;}
-			img.mangafox		{width:130px;
-								 height:110px;}
-<style/>
-<h1>Exercise 6.4</h1>								 
-<h2><?php echo $name; ?></h2>
- 
+<h1>Exercise 6.4</h1>	
+
+
+<p>Hobbies:</p>
+					<img class="basketball" src="basketball.jpg" alt="Play Basketball"/>
+					<img class="video" src="controller.jpg" alt="Play video games"/>
+					<img class="books" src="book.jpg" alt="Read books"/> 
+					<img class="food" src="food.jpg" alt="Eat Food"/>
+					<img class="dog" src="dog.jpg" alt="Dogs"/>
+					<img class="friends" src="friends.gif" alt="Hanging with friends"/>
+					<img class="dance" src="pop.jpg" alt="A little of dancing"/>
+					<img class="csgo" src="csgo.jpg" alt="Counter-Strike: Global Offensive"/>
+			<br/></br>
+				<p>Favorite Websites:</p>	
+					<a href="http://youtube.com"><img class="youtube" src="youtube.png" alt="Youtube Icon"/> </a>
+					<a href="http://google.com"><img class="google" src="google.png" alt="Google Icon"/> </a>
+					<a href="http://reddit.com"><img class="reddit" src="reddit.jpg" alt="Reddit Icon" /> </a>
+					<a href="http://nba.com"><img class="nba" src="nba.jpeg" alt="NBA Icon" /> </a>
+					<a href="http://hltv.org"><img class="hltv" src="hltv.png" alt="HLTV Icon" /> </a>
+					<a href="http://facebook.com"><img class="facebook" src="facebook.png" alt="Facebook Icon" /> </a>
+					<a href="http://9gag.com"><img class="gag" src="9gag.png" alt="9gag Icon" /> </a>
+					<a href="http://moodle2.apc.edu.ph"><img class="moodle" src="moodle.png" alt="Moodle Icon" /> </a>
+					<a href="http://mangafox.com"><img class="mangafox" src="mangafox.png" alt="Mange Icon" /> </a>
+					<p>Click on icon to see websites!</p></br>							 
+
 <table border='1' cellpadding='9'>
     <tr>
         <td><strong>Name</strong></td>
