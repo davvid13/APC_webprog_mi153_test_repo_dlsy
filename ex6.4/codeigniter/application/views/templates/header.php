dfd527f0eac6451fd2bf3675acecd5c72eb3e4c9
Davvid <davvid19996@gmail.com>
2016-12-13 12:39:26 +0800

    Added Hobbies: Basketball Food video books dogs frineds dance csgo

--- ex6.4/codeigniter/application/views/templates/header.php
+++ ex6.4/codeigniter/application/views/templates/header.php
@@ -70,3 +70,15 @@
 					<th>Davvid</th>
 				</tr>	
 			</table>
+			
+			
+				<p>Hobbies:</p>
+					<img class="basketball" src="basketball.jpg" alt="Play Basketball"/>
+					<img class="video" src="controller.jpg" alt="Play video games"/>
+					<img class="books" src="book.jpg" alt="Read books"/> 
+					<img class="food" src="food.jpg" alt="Eat Food"/>
+					<img class="dog" src="dog.jpg" alt="Dogs"/>
+					<img class="friends" src="friends.gif" alt="Hanging with friends"/>
+					<img class="dance" src="pop.jpg" alt="A little of dancing"/>
+					<img class="csgo" src="csgo.jpg" alt="Counter-Strike: Global Offensive"/>
+			<br/></br>
