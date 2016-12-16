8db1acdeb6bbd86357b13e19b40e72ed5dbebef0
Davvid <davvid19996@gmail.com>
2016-12-16 11:58:31 +0800

    Edited image path of the pictures used in ex6.4/codeigniter/application/views/news/index.php
    Changed the location of the image folder
    
    Extracted Yii files to ex7 folder

--- ex6.4/codeigniter/application/views/news/index.php
+++ ex6.4/codeigniter/application/views/news/index.php
@@ -4,14 +4,14 @@
 
 
 <p>Hobbies:</p>
-					<img class="basketball" src="basketball.jpg" alt="Play Basketball"/>
-					<img class="video" src="controller.jpg" alt="Play video games"/>
-					<img class="books" src="book.jpg" alt="Read books"/> 
-					<img class="food" src="food.jpg" alt="Eat Food"/>
-					<img class="dog" src="dog.jpg" alt="Dogs"/>
-					<img class="friends" src="friends.gif" alt="Hanging with friends"/>
-					<img class="dance" src="pop.jpg" alt="A little of dancing"/>
-					<img class="csgo" src="csgo.jpg" alt="Counter-Strike: Global Offensive"/>
+					<img class="basketball" src="/image/basketball.jpg" alt="Play Basketball"/>
+					<img class="video" src="/image/controller.jpg" alt="Play video games"/>
+					<img class="books" src="/images/book.jpg" alt="Read books"/> 
+					<img class="food" src="/images/food.jpg" alt="Eat Food"/>
+					<img class="dog" src="/images/dog.jpg" alt="Dogs"/>
+					<img class="friends" src="/images/friends.gif" alt="Hanging with friends"/>
+					<img class="dance" src="/images/pop.jpg" alt="A little of dancing"/>
+					<img class="csgo" src="/images/csgo.jpg" alt="Counter-Strike: Global Offensive"/>
 			<br/></br>
 				<p>Favorite Websites:</p>	
 					<a href="http://youtube.com"><img class="youtube" src="youtube.png" alt="Youtube Icon"/> </a>
