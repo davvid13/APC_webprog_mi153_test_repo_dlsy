9088b3d2cd74f850486d94b8d833821da366f95d
Davvid <davvid19996@gmail.com>
2016-12-16 12:58:44 +0800

    1 picture working WOOO

--- ex6.4/codeigniter/application/views/news/index.php
+++ ex6.4/codeigniter/application/views/news/index.php
@@ -4,7 +4,7 @@
 
 
 <p>Hobbies:</p>
-					<img class="basketball" alt="Play Basketball" src="<?php echo site_url('/image/basketball.jpg'); ?> />
+					<img class="basketball" alt="Play Basketball" src="<?php echo base_url('/image/basketball.jpg'); ?>" />
 					<img class="video" src="/image/controller.jpg" alt="Play video games"/>
 					<img class="books" alt="Read books" <?php echo site_url('/image/book.jpg'); ?>/> 
 					<img class="food" alt="Eat Food" <?php echo site_url('/image/food.jpg'); ?>/>
@@ -14,7 +14,7 @@
 					<img class="csgo" alt="Counter-Strike: Global Offensive" <?php echo site_url('/image/csgo.jpg'); ?>/>
 			<br/></br>
 				<p>Favorite Websites:</p>	
-					<a href="http://youtube.com"><img class="youtube" alt="Youtube Icon" <?php echo site_url('/images/youtube.png'); ?>/> </a>
+					<a href="http://youtube.com"><img class="youtube" alt="Youtube Icon" <?php echo base_url('/images/youtube.png'); ?>/> </a>
 					<a href="http://google.com"><img class="google" alt="Google Icon"<?php echo site_url('/images/google.png'); ?>/> </a>
 					<a href="http://reddit.com"><img class="reddit" alt="Reddit Icon" <?php echo site_url('/images/reddit.jpg'); ?>/> </a>
 					<a href="http://nba.com"><img class="nba" alt="NBA Icon" <?php echo site_url('/images/nba.jpg'); ?>/> </a>
