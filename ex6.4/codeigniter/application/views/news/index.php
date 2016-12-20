5f2e193260e52c9f48c68e42633fc83eaea639ea
davvid13 <davvid19996@gmail.com>
2016-12-20 20:30:21 +0800

    Edited code
    edited food picture code

--- ex6.4/codeigniter/application/views/news/index.php
+++ ex6.4/codeigniter/application/views/news/index.php
@@ -6,8 +6,8 @@
 <p>Hobbies:</p>
 					<img class="basketball" alt="Play Basketball" src="<?php echo base_url('/image/basketball.jpg'); ?>" />
 					<img class="video" alt="Play video games" src="<?php echo base_url('/image/controller.jpg'); ?>"/>
-					<img class="books" alt="Read books" <?php echo site_url('/image/book.jpg'); ?>/> 
-					<img class="food" alt="Eat Food" <?php echo site_url('/image/food.jpg'); ?>/>
+					<img class="books" alt="Read books" src="<?php echo base_url('/image/books.jpg'); ?>"/> 
+					<img class="food" alt="Eat Food" src="<?php echo base_url('/image/food.jpg'); ?>"/>
 					<img class="dog" alt="Dogs" <?php echo site_url('/image/dog.jpg'); ?>/>
 					<img class="friends" alt="Hanging with friends" <?php echo site_url('/image/friends.gif'); ?>/>
 					<img class="dance" alt="A little of dancing" <?php echo site_url('/image/pop.jpg'); ?>/>
